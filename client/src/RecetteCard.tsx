9694739261b0180a8cf4a166f0d50cbcec3baf80
BELHASSEN <julia_belhassen@hotmail.com>
2024-11-08 15:50:47 +0100

    Recipe Card component is in progress/adding CSS

--- client/src/RecetteCard.tsx
+++ client/src/RecetteCard.tsx
@@ -1,8 +1,11 @@
+import "./assets/style/RecetteCard.css";
+
 interface RecetteCardId {
   imgSrc: string;
   recetteTitle: string;
   cardStars: string;
   cardHeart: string;
+  cardCart: string;
 }
 
 function RecetteCard({
@@ -10,14 +13,16 @@ function RecetteCard({
   recetteTitle,
   cardStars,
   cardHeart,
+  cardCart,
 }: RecetteCardId) {
   return (
     <>
-      <section>
-        <figure>{imgSrc}</figure>
+      <section className="recetteCard">
+        <img src={imgSrc} alt={"Food representing the recipe"} />
         <h3>{recetteTitle}</h3>
-        <p>{cardStars}</p>
-        <p>{cardHeart}</p>
+        <img src={cardStars} alt={"Recipe difficulty"} className="starImg" />
+        <button type={undefined}>{cardHeart}</button>
+        <button type={undefined}>{cardCart}</button>
       </section>
     </>
   );
