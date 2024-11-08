669808b7ae70969271ae20f9754e0912bd5937fd
GaeBolga <benjamin.lefort@mail.com>
2024-11-08 12:03:39 +0100

    3 buttons and 3 form where added to the component

--- client/src/components/US01-Overlay-Form.tsx
+++ client/src/components/US01-Overlay-Form.tsx
@@ -1,13 +1,66 @@
-function OverlayForm() {
+import { useState } from "react";
+
+function FormWithButton() {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsFormVisible(true);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    alert("Formulaire soumis !");
+  };
+
   return (
-    <>
-      <form className="overlay-form">
-        <button type="button" onClick={() => console.log("ferme ton bouton")}>
-          image croix
+    <div>
+      <button type="button" onClick={handleButtonClick}>
+        Ouvrir le formulaire
+      </button>
+      {isFormVisible && (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <p>envie d'une recette particulière ?</p>
+            <label htmlFor="username"> </label>
+            <input
+              placeholder="Tapez le nom de votre recette :"
+              type="text"
+              id="username"
+              name="username"
+            />
+            <div className="item-choice">
+              <p>Ingredient n°1</p>
+              <label htmlFor="username"> </label>
+              <input
+                placeholder="carrote"
+                type="text"
+                id="username"
+                name="username"
+              />
+              <p>Ingredient n°2</p>
+
+              <label htmlFor="username"> </label>
+              <input
+                placeholder="mouton"
+                type="text"
+                id="username"
+                name="username"
+              />
+            </div>
+          </div>
+          <button type="submit">Envoyer</button>
+        </form>
+      )}
+      <div>
+        <button type="button" onClick={handleSubmit}>
+          Ouvrir le formulaire
         </button>
-      </form>
-    </>
+        <button type="button" onClick={handleSubmit}>
+          Ouvrir le formulaire
+        </button>
+      </div>
+    </div>
   );
 }
 
-export default OverlayForm;
+export default FormWithButton;
