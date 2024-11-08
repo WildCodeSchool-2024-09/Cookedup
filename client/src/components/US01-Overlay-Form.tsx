e100fa0e0b46253fb64d1783da7841aeaa648d9c
GaeBolga <benjamin.lefort@mail.com>
2024-11-08 11:09:18 +0100

    Initialization first component and testing a first push with biome

---
+++ client/src/components/US01-Overlay-Form.tsx
@@ -0,0 +1,13 @@
+function OverlayForm() {
+  return (
+    <>
+      <form className="overlay-form">
+        <button type="button" onClick={() => console.log("ferme ton bouton")}>
+          image croix
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default OverlayForm;
