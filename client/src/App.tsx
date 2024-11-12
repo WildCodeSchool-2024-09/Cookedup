47a2d44cedd8daa9e607a528a30891ddce213992
BELHASSEN <julia_belhassen@hotmail.com>
2024-11-12 10:56:34 +0100

    Recipe card CSS is in progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,35 @@
-import "./App.css";
+import { useState } from "react";
+import RecetteCard from "./RecetteCard";
+import RecettesList from "./RecettesArray";
+import "./assets/style/RecetteCard.css";
 
 function App() {
-  return <></>;
+  // uitliser deux useState : un "Favoris" et un "Panier"
+  const [mesFavoris, setMesFavoris] = useState([]);
+
+  function addFavorite() {
+    setMesFavoris(mesFavoris);
+  }
+
+  return (
+    <>
+      {/* <RecetteCard 
+    imgSrc={RecettesList[0].imgSrc} 
+    recetteTitle={RecettesList[0].recetteTitle} 
+    cardStars={RecettesList[0].cardStars} 
+    cardHeart={RecettesList[0].cardHeart}/> */}
+
+      <section className="recetteListe">
+        {RecettesList.map((value) => (
+          <RecetteCard
+            key={value.recetteTitle}
+            {...value}
+            addFavorite={addFavorite}
+          />
+        ))}
+      </section>
+    </>
+  );
 }
 
 export default App;
--- client/src/RecetteCard.tsx
+++ client/src/RecetteCard.tsx
@@ -6,6 +6,7 @@ interface RecetteCardId {
   cardStars: string;
   cardHeart: string;
   cardCart: string;
+  addFavorite: (arg0: string) => void;
 }
 
 function RecetteCard({
@@ -14,14 +15,21 @@ function RecetteCard({
   cardStars,
   cardHeart,
   cardCart,
+  addFavorite,
 }: RecetteCardId) {
   return (
     <>
       <section className="recetteCard">
-        <img src={imgSrc} alt={"Food representing the recipe"} />
+        <img
+          src={imgSrc}
+          alt={"Food representing the recipe"}
+          className="foodImg"
+        />
         <h3>{recetteTitle}</h3>
         <img src={cardStars} alt={"Recipe difficulty"} className="starImg" />
-        <button type={undefined}>{cardHeart}</button>
+        <button onClick={() => addFavorite(recetteTitle)} type={undefined}>
+          {cardHeart}
+        </button>
         <button type={undefined}>{cardCart}</button>
       </section>
     </>
