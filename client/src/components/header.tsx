c35c927204445d0cf901eca9782281e6e80a2d4d
Marcellin123 <marcellindikelel@yahoo.fr>
2024-11-13 10:15:05 +0100

    Header CSS in progress

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -3,25 +3,36 @@ import "../assets/styles/header.css";
 function Header() {
   return (
     <div>
-      <figure id="home">
-        <img src="/src/assets/image/image-temp.webp" alt="" />
+      <figure>
+        <img id="home" src="/src/assets/image/image-temp.webp" alt="cuisine" />
       </figure>
-      <figure id="left-flower">
-        <img src="/src/assets/image/Pngtreeivy_vines_926036.webp" alt="" />
-      </figure>
-      <figure id="right-flower">
-        <img
-          src="/src/assets/image/vine-computer-icons-clip-art-ivy-53f2b07d5459bc3cc8cd0ee5dda0630a.webp"
-          alt=""
-        />
-      </figure>
+      <div className="Flower">
+        <figure>
+          <img
+            id="left-flower"
+            src="/src/assets/image/Pngtreeivy_vines_926036.webp"
+            alt="Fleur située à droite"
+          />
+        </figure>
+        <figure>
+          <img
+            id="right-flower"
+            src="/src/assets/image/vine-computer-icons-clip-art-ivy-53f2b07d5459bc3cc8cd0ee5dda0630a.webp"
+            alt="Fleur située à gauche"
+          />
+        </figure>
+      </div>
       <p>
         Bienvenue chez Cooked-Up !Ici, cuisiner devient facile et amusant.Si une
         recette te plaît, clique sur le cœur pour l’ajouter à tes favoris.
         Prêt(e) à cuisiner ? Ajoute ta recette à ta liste de courses !
       </p>
-      <figure id="logo">
-        <img src="/src/assets/image/CookedUpLogo_2.webp" alt="" />
+      <figure>
+        <img
+          id="logo"
+          src="/src/assets/image/CookedUpLogo_2.webp"
+          alt="Logo de l'application"
+        />
       </figure>
     </div>
   );
