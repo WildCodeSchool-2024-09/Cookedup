f02195a5d358a350ec0f3d1737ce8a8e6fd93869
BELHASSEN <julia_belhassen@hotmail.com>
2024-11-22 09:58:19 +0100

    Nothing

--- client/src/components/Aside.tsx
+++ client/src/components/Aside.tsx
@@ -10,9 +10,9 @@ function Aside({ imgSrc }: AsideProps) {
       <header className="main-aside">
         <img className="cookedup-logo" src={imgSrc} alt="Cooked Up logo" />
         <nav className="aside-nav">
-          <NavLink to ="" >Connection / Inscription </NavLink>
+          {/* <NavLink to ="" >Connection / Inscription </NavLink>
           <NavLink to="">Mes recettes favorites</NavLink>
-          <NavLink to="">Ma shopping liste</NavLink>
+          <NavLink to="">Ma shopping liste</NavLink> */}
         </nav>
       </header>
     </>
